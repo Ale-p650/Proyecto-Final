37f1a2bf2efda37d3aa9a09253ac830b502c9419
Ale-p650 <ale.pavlinusich@gmail.com>
2022-09-02 10:24:18 -0300

    CRUD completo de Productos implementado
    Se "pulieron" y testearon los metodos CrearProducto, ModificarProducto,TraerProductos, EliminarProducto, que ya estaban creados del proyecto anterior

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Proyecto_Final.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
 
     public class ProductoController : ControllerBase
     {
